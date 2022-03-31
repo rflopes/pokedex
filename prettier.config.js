bf6a96dea71432da5d709d33fa1f52243613062f
Ricardo Lopes <mail@ricardolopes.pt>
2022-03-31 11:44:58 +0100

    update prettier configuration

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  singleQuote: true,
-  semi: false,
+  "arrowParens": "always",
+  "bracketSameLine": false,
+  "bracketSpacing": true,
+  "trailingComma": "es5",
+  "jsxSingleQuote": false,
+  "quoteProps": "consistent",
+  "singleQuote": false,
+  "semi": true,
+  "useTabs": false,
+  "tabWidth": 4,
+  "printWidth": 80
 }
